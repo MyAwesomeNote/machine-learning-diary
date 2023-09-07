4ab9829723fd957d794d8628b3182d25babd35b3
PleahMaCaka <pleahmacaka@gmail.com>
2023-09-07 15:24:54 +0900

    Add missing ReLU

--- day4/cnn.py
+++ day4/cnn.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torchvision
 from torch.autograd import Variable
+from torch.nn import functional as nn_func
 from torch.utils.data import DataLoader
 
 print("CUDA is", "Enabled" if torch.cuda.is_available() else "Not enabled")
@@ -36,31 +37,31 @@ plt.show()
 
 class FashionCNN(nn.Module):
     def __init__(self):
-        super(FashionCNN, self).__init__()
+        super().__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(num_features=32),
+            nn.Conv2d(1, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(2, 2)
         )
         self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
-            nn.BatchNorm2d(num_features=64),
+            nn.Conv2d(32, 64, 3),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
-        self.fc1 = nn.Linear(in_features=64 * 6 * 6, out_features=600)
-        self.drop = nn.Dropout(0.25)
-        self.fc2 = nn.Linear(in_features=600, out_features=120)
-        self.fc3 = nn.Linear(in_features=120, out_features=10)
+        self.fc1 = nn.Linear(int(32 * (48 + 1) / 2), 600)  # 784
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(600, 120)
+        self.fc3 = nn.Linear(120, 10)
 
-    def forward(self, x):
-        out = self.layer1(x)
+    def forward(self, input_data):
+        out = self.layer1(input_data)
         out = self.layer2(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc1(out)
+        out = input_data.view(out.size(0), -1)
+        out = nn_func.relu(self.fc1(out))
         out = self.drop(out)
-        out = self.fc2(out)
+        out = nn_func.relu(self.fc2(out))
         out = self.fc3(out)
         return out
 
